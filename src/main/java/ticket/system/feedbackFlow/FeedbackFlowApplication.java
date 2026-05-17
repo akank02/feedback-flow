5450070cf1cf239be266815977321c8e1c59f598
Akanksha Arora <akanksha02arora@gmail.com>
2026-05-17 20:55:09 +0530

    chore: initial Spring Boot project setup

---
+++ src/main/java/ticket/system/feedbackFlow/FeedbackFlowApplication.java
@@ -0,0 +1,13 @@
+package ticket.system.feedbackFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ticket/system/feedbackFlow/FeedbackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package ticket.system.feedbackFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
